a38e9bafd6fb0d3fffc539a454a3cb3127f76a38
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-07-01 11:28:07 +0530

    Update memberRoutes.js

--- backend/routes/memberRoutes.js
+++ backend/routes/memberRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { getMembers, filterMembers } = require('../controllers/memberController');
+const { getMembers, filterMembers, updateProfilePicture } = require('../controllers/memberController');
+const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/', getMembers);
 router.post('/filter', filterMembers);
+router.post('/uploadProfilePicture', authMiddleware, updateProfilePicture);
 
 module.exports = router;
