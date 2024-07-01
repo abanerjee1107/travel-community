4e44b6a9377bbbcb3b1d6f63c724933848481fdc
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-07-01 12:28:05 +0530

    Update tripController.js

--- backend/controllers/tripController.js
+++ backend/controllers/tripController.js
@@ -1,4 +1,6 @@
 const Trip = require('../models/Trip');
+const { Client } = require('@googlemaps/google-maps-services-js');
+const client = new Client({});
 
 exports.createTrip = (req, res) => {
   const { title, description, date, userId } = req.body;
@@ -13,4 +15,23 @@ exports.getTrips = (req, res) => {
     if (err) return res.status(500).json({ error: err.message });
     res.status(200).json({ data: results });
   });
+};
+
+exports.getTripDetails = (req, res) => {
+  const { destination } = req.params;
+  client
+    .places({
+      params: {
+        input: destination,
+        inputtype: 'textquery',
+        key: process.env.GOOGLE_MAPS_API_KEY,
+      },
+      timeout: 1000, // milliseconds
+    })
+    .then((r) => {
+      res.status(200).send(r.data);
+    })
+    .catch((e) => {
+      res.status(500).send(e.response.data.error_message);
+    });
 };
