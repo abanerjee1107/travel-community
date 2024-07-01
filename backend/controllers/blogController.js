c62a27ed25d4ba815232bdbf803db4f13aeb8b90
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-07-01 15:40:48 +0530

    Update blogController.js

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,5 +1,6 @@
 const Blog = require('../models/Blog');
 
+// Original exports for creating and fetching blogs
 exports.createBlog = (req, res) => {
     const { title, content, categories } = req.body;
     const newBlog = new Blog({ title, content, categories });
@@ -22,4 +23,40 @@ exports.getBlogsByCategory = (req, res) => {
         if (err) return res.status(500).send(err);
         res.status(200).send(blogs);
     });
+};
+
+// New exports for creating and fetching blog posts
+const createBlogPost = async (req, res) => {
+    const { title, content } = req.body;
+    try {
+        const newBlogPost = new Blog({
+            title,
+            content,
+            author: req.user._id // Assuming authenticated user is the author
+        });
+        await newBlogPost.save();
+        res.status(201).json({ message: 'Blog post created successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+const getBlogPosts = async (req, res) => {
+    try {
+        const blogPosts = await Blog.find().populate('author', 'username');
+        res.status(200).json(blogPosts);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+// Exporting all functions together
+module.exports = {
+    createBlog,
+    getBlogs,
+    getBlogsByCategory,
+    createBlogPost,
+    getBlogPosts,
 };
