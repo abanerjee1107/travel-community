616a432c2df21a9b2ee78675db72bceb7f027d5c
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-07-01 11:03:36 +0530

    Create Blog.js

---
+++ backend/models/Blog.js
@@ -0,0 +1,15 @@
+const db = require('../config/db');
+
+const Blog = {
+  create: (blog, callback) => {
+    const { title, content, authorId } = blog;
+    const query = 'INSERT INTO blogs (title, content, author_id) VALUES (?, ?, ?)';
+    db.query(query, [title, content, authorId], callback);
+  },
+  getAll: (callback) => {
+    const query = 'SELECT * FROM blogs';
+    db.query(query, callback);
+  },
+};
+
+module.exports = Blog;
