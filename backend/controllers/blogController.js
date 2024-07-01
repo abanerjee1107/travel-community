9faa322e68e2c962ac31aeb3b880172bafb0ffb6
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-07-01 12:10:12 +0530

    Update blogController.js

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,16 +1,25 @@
 const Blog = require('../models/Blog');
 
 exports.createBlog = (req, res) => {
-  const { title, content, authorId } = req.body;
-  Blog.create({ title, content, authorId }, (err, result) => {
-    if (err) return res.status(500).json({ error: err.message });
-    res.status(201).json({ message: 'Blog post created successfully', data: result });
-  });
+    const { title, content, categories } = req.body;
+    const newBlog = new Blog({ title, content, categories });
+    newBlog.save((err, blog) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).send(blog);
+    });
 };
 
 exports.getBlogs = (req, res) => {
-  Blog.getAll((err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
-    res.status(200).json({ data: results });
-  });
+    Blog.find({}, (err, blogs) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).send(blogs);
+    });
+};
+
+exports.getBlogsByCategory = (req, res) => {
+    const { category } = req.params;
+    Blog.find({ categories: category }, (err, blogs) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).send(blogs);
+    });
 };
