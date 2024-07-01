ff73100a0192c6ee395999e767141505559d9bcb
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-07-01 15:42:07 +0530

    Update Blog.js

--- frontend/components/Blog.js
+++ frontend/components/Blog.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Blog = ({ blog }) => (
   <div>
@@ -8,4 +9,30 @@ const Blog = ({ blog }) => (
   </div>
 );
 
-export default Blog;
+const BlogContainer = () => {
+    const [blogPosts, setBlogPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchBlogPosts = async () => {
+            try {
+                const response = await axios.get('/api/blog/posts');
+                setBlogPosts(response.data);
+            } catch (error) {
+                console.error('Error fetching blog posts:', error);
+            }
+        };
+
+        fetchBlogPosts();
+    }, []);
+
+    return (
+        <div>
+            <h2>Travel Advice and Tips</h2>
+            {blogPosts.map((post) => (
+                <Blog key={post._id} blog={post} />
+            ))}
+        </div>
+    );
+};
+
+export default BlogContainer;
