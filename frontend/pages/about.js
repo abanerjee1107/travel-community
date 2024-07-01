8e53ff4ccbd7276b5137c0ec89170288175258b8
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-07-01 11:16:49 +0530

    Create about.js

---
+++ frontend/pages/about.js
@@ -0,0 +1,8 @@
+const About = () => (
+  <div>
+    <h1>About Us</h1>
+    <p>We are a community of travelers who love to explore the world and share our experiences.</p>
+  </div>
+);
+
+export default About;
