b8d1659d0ff30275b974e410bfd2c9f4b428afd0
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-07-01 15:22:39 +0530

    Update home.js

--- frontend/pages/home.js
+++ frontend/pages/home.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Container, Typography, Button } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 const Home = () => {
+    const { t } = useTranslation();
+
     return (
         <Container>
             <Typography variant="h4" component="h1" gutterBottom>
-                Welcome to Travel Community!
+                {t('welcomeTitle')}
             </Typography>
             <Typography variant="body1" gutterBottom>
-                Connect with like-minded travelers and plan your next adventure.
+                {t('welcomeMessage')}
             </Typography>
             <Button variant="contained" color="primary">
-                Get Started
+                {t('getStarted')}
             </Button>
         </Container>
     );
 };
 
 export default Home;
+
