e5e876ba6ae27690ee40230be95ccd349eaf16f9
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-07-01 11:05:22 +0530

    Create messagingRoutes.js

---
+++ backend/routes/messagingRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { sendMessage, getMessages } = require('../controllers/messagingController');
+const router = express.Router();
+
+router.post('/send', sendMessage);
+router.get('/:userId', getMessages);
+
+module.exports = router;
