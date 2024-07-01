5b281bf18f7ac72d1c16485b03410e8468d0e016
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-07-01 11:00:53 +0530

    Create index.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Welcome to the Travel Community</h1>
+      <p>Connect with like-minded travelers and plan your trips.</p>
+    </Layout>
+  );
+}
