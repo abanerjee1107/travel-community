bb742663e4881e69920664f657bffced124d02af
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-07-01 12:47:01 +0530

    Update tripRoutes.js

--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { createTrip, getTrips } = require('../controllers/tripController');
+const tripController = require('../controllers/tripController');
 const router = express.Router();
 
+// Routes for individual trips
 router.post('/create', createTrip); // POST request to create a new trip
 router.get('/', getTrips);           // GET request to fetch all trips
 
+// Routes for group trips
+router.post('/createGroupTrip', tripController.createGroupTrip);
+router.get('/groupTrips', tripController.getGroupTrips);
+
 module.exports = router;
-
-
