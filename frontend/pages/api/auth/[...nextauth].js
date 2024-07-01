b85843ce9bb0d7986b2a6a3667c843db45db323e
abanerjee1107 <123304285+abanerjee1107@users.noreply.github.com>
2024-07-01 11:01:53 +0530

    Create [...nextauth].js

---
+++ frontend/pages/api/auth/[...nextauth].js
@@ -0,0 +1,34 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { verifyPassword } from '../../../utils/auth';
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+      },
+      authorize: async (credentials) => {
+        // Implement your own login logic here
+        // Validate user credentials, etc.
+      }
+    })
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.id = user.id;
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id;
+      return session;
+    }
+  }
+});
